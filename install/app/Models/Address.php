adbdf8e07244ba3093bd3080000287459aaf606f
Sanket Mane <sanketpatil515151@gmail.com>
2026-03-09 17:54:17 +0530

    Fix: Increased postal_code column length in addresses table to 255 to resolve Telecaller biodata submission error - Sanket

--- install/app/Models/Address.php
+++ install/app/Models/Address.php
@@ -9,7 +9,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Address extends Model
 {
     use SoftDeletes;
-    protected $fillable = ['user_id', 'type', 'country_id', 'state_id', 'city_id'];
+    protected $fillable = ['user_id', 'type', 'country_id', 'state_id', 'city_id', 'postal_code'];
 
     public function user()
     {
