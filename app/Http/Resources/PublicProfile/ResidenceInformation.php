cb62615db032cc514a6ed0657b952a540c74536d
Sanket Mane <sanketpatil515151@gmail.com>
2026-03-10 20:59:38 +0530

    Fix null check in root resource files as well [Sanket]

--- app/Http/Resources/PublicProfile/ResidenceInformation.php
+++ app/Http/Resources/PublicProfile/ResidenceInformation.php
@@ -15,9 +15,14 @@ class ResidenceInformation extends JsonResource
      */
     public function toArray($request)
     {
-        $birth_country = $this->birth_country_id ? Country::where('id', $this->birth_country_id)->first()->name : null;
-        $recidency_country = $this->recidency_country_id ? Country::where('id', $this->recidency_country_id)->first()->name : null;
-        $growup_country = $this->growup_country_id ? Country::where('id', $this->growup_country_id)->first()->name : null;
+        $birth_country_obj = $this->birth_country_id ? Country::find($this->birth_country_id) : null;
+        $birth_country = $birth_country_obj ? $birth_country_obj->name : null;
+
+        $recidency_country_obj = $this->recidency_country_id ? Country::find($this->recidency_country_id) : null;
+        $recidency_country = $recidency_country_obj ? $recidency_country_obj->name : null;
+
+        $growup_country_obj = $this->growup_country_id ? Country::find($this->growup_country_id) : null;
+        $growup_country = $growup_country_obj ? $growup_country_obj->name : null;
         return [
             'birth_country' => $birth_country,
             'recidency_country' => $recidency_country,
