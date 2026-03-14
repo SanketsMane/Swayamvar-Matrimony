a80247d92a7e5289295eb0ffb3626126159aabf0
Sanket Mane <sanket@swayamvarmatrimony.in>
2026-03-15 03:47:44 +0530

    Release: Flutter Web Build v1.2

--- Matrimony_App/build/web/flutter_bootstrap.js
+++ Matrimony_App/build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"e4b8dca3f1b4ede4c30371002441c88c12187e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1985494357" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1447772107" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
